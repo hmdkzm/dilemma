43898d83fe2c692d35f34152c004ac2bfbdf014f
hmdkzm <surrmind@gmail.com>
2026-07-14 23:40:42 +0200

    feat: add abyss hazard to rooms
    - Created animated Abyss hazard with fall sfx
    - Implemented player consumption animation
    - Placed abyss hazards in room_a7 and room_b9
    - Adjusted default audio settings to 50% volume
    - Updated title screen fade-in animation length

--- scenes/splash/title.gd
+++ scenes/splash/title.gd
@@ -3,7 +3,7 @@ extends Node2D
 
 func _ready():
 	# Play the fade-in animation
-	$AnimationPlayer.speed_scale = 0.5
+	$AnimationPlayer.speed_scale = 1
 	$AnimationPlayer.play("fade_in")
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
